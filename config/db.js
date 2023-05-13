dbcb8df91d44aec5241bce2788b41dbe3802ac17
vasylmiskiv <vasylmiskiv@gmail.com>
2023-05-13 11:36:19 +0300

    server folder from https://github.com/vasylmiskiv/fullstack-ecommerceapp for service deployment

---
+++ config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const link = await mongoose.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log(
+      "MongoDB connected ".blue + link.connection.host.cyan.underline
+    );
+  } catch (error) {
+    console.error("Error " + error.message.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
